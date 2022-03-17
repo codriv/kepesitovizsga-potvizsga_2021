625405e28512fb3f8ad44d48c1edc931861efb46
codriv <combodriver4@yahoo.at>
2022-03-17 19:06:32 +0100

    Forest - patch - removeIf

--- src/main/java/hu/nive/ujratervezes/forest/Forest.java
+++ src/main/java/hu/nive/ujratervezes/forest/Forest.java
@@ -1,6 +1,7 @@
 package hu.nive.ujratervezes.forest;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,8 +22,21 @@ public class Forest {
     }
 
     public void cutTrees(Lumberjack lumberjack) {
+        trees.removeIf(lumberjack::canCut);
+    }
+
+    public void cutTrees2(Lumberjack lumberjack) {
         List<Tree> treesToCut = trees.stream().filter(lumberjack::canCut).collect(Collectors.toList());
         trees.removeAll(treesToCut);
+    }
+
+    public void cutTrees3(Lumberjack lumberjack) {
+        Iterator iterator = trees.iterator();
+        while (iterator.hasNext()) {
+            if(lumberjack.canCut((Tree)iterator.next())) {
+                iterator.remove();
+            }
+        }
     }
 
     public boolean isEmpty() {
