65794a0cf22f1d8af648f2bc9c0592654be47d1e
codriv <combodriver4@yahoo.at>
2022-03-17 17:06:17 +0100

    WebshopService

---
+++ src/main/java/hu/nive/ujratervezes/webshop/WebshopService.java
@@ -0,0 +1,35 @@
+package hu.nive.ujratervezes.webshop;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebshopService {
+
+    private MariaDbDataSource dataSource;
+
+    public WebshopService(MariaDbDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<String> getValuableCustomers() {
+        try(Connection con = dataSource.getConnection();
+            Statement stmt = con.createStatement()) {
+            try(ResultSet rs = stmt.executeQuery("select customer_name from orders where total > 1000 order by customer_name")) {
+                List<String> customers = new ArrayList<>();
+                while (rs.next()) {
+                    String customer = rs.getString("customer_name");
+                    customers.add(customer);
+                }
+                return customers;
+            }
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Cannot reach database!");
+        }
+    }
+}
